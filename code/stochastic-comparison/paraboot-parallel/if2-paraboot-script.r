6169e647711d462871aefbb8ce5a745c9622352f
Dexter Barrows <dex.barrows@gmail.com>
2016-02-02 01:12:14 -0500

    IF2 parametric bootstrapping in separate file

---
+++ code/stochastic-comparison/paraboot-parallel/if2-paraboot-script.r
@@ -0,0 +1,91 @@
+# Dexter Barrows
+#
+# run: Rscript if2-paraboot-script.r > if2-paraboot-script.log 2>&1
+
+library(foreach)
+library(parallel)
+library(doParallel)
+library(ggplot2)
+library(Rcpp)
+
+set.seed(8265813)
+
+# R source files
+stoc_sir_file 		<- paste(getwd(),"../../sir-functions", "StocSIR.r", sep = "/")
+if2_paraboot_file 	<- paste(getwd(), "if2-paraboot.r", sep = "/")
+source(stoc_sir_file)
+source(if2_paraboot_file)
+
+# C++ source files
+if2file <- paste(getwd(),"../../if2", "if2.cpp", sep="/")
+if2_s_file <- paste(getwd(),"../../if2", "if2-s.cpp", sep="/")
+
+## data params
+T       <- 60
+i_infec <- 5
+steps   <- 7
+N       <- 500
+sigma   <- 10
+Tlim    <- round(T * 2/3)
+
+## Generate true trajecory and synthetic data
+##
+
+pars_true <- c(R0 = 3.0,    # new infected people per infected person
+          r = 0.1,      # recovery rate
+          N = 500,      # population size
+          eta = 0.5,    # geometric random walk
+          berr = 0.5)   # Beta geometric walk noise
+
+true_init_cond <- c(S = N - i_infec,
+                    I = i_infec,
+                    R = 0)
+
+# get true trajectory
+sdeout_true <- StocSIR(true_init_cond, pars_true, T, steps)
+colnames(sdeout_true) <- c('S','I','R','B')
+
+# get initial data
+infec_counts_raw <- sdeout_true[,'I'] + rnorm(T+1, 0, sigma)
+infec_counts     <- ifelse(infec_counts_raw < 0, 0, infec_counts_raw)
+
+## parallel setup
+##
+
+nTrials <- 2
+
+numCores <- detectCores()
+cl <- makeCluster(min(numCores, nTrials))
+registerDoParallel(cl)
+
+## if2 setup
+##
+
+NP          <- 3000
+nPasses     <- 50
+coolrate    <- 0.975
+
+## initial fit
+##
+
+
+
+#if (FALSE) {
+sourceCpp(if2file)
+if2time <- system.time( if2data <- if2(infec_counts[1:(Tlim+1)], Tlim+1, N, NP, nPasses, coolrate) )
+
+# get parameter estimates
+
+paramdata <- data.frame( if2data$paramdata )
+names(paramdata) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
+parmeans <- colMeans(paramdata)
+names(parmeans) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
+
+if2_paraboot_data <- if2_paraboot(if2data,
+                                  T, Tlim, steps, N, nTrials,
+                                  if2file, if2_s_file, stoc_sir_file,
+                                  NP, nPasses, coolrate)
+
+stopCluster(cl)
+
+save.image(file = "if2-paraboot.RData")
--- code/stochastic-comparison/paraboot-parallel/if2-paraboot.r
+++ code/stochastic-comparison/paraboot-parallel/if2-paraboot.r
@@ -1,133 +1,101 @@
 # Dexter Barrows
 #
-# run: Rscript if2-paraboot.r > if2-paraboot.log 2>&1
+# IF2 parametric bootstrapping function
 
 library(foreach)
 library(parallel)
 library(doParallel)
-library(ggplot2)
 library(Rcpp)
 
-source(paste(getwd(),"../../sir-functions", "StocSIR.r", sep = "/"))
+if2_paraboot <- function(if2data, T, Tlim, steps, N, nTrials, if2file, if2_s_file, stoc_sir_file, NP, nPasses, coolrate) {
 
-set.seed(112358)
+	source(stoc_sir_file)
 
-
-## data params
-T       <- 60
-i_infec <- 5
-steps   <- 7
-N       <- 500
-sigma   <- 10
-Tlim    <- round(T / 2)
-
-## Generate true trajecory and synthetic data
-##
-
-pars_true <- c(R0 = 3.0,    # new infected people per infected person
-          r = 0.1,      # recovery rate
-          N = 500,      # population size
-          eta = 0.5,    # geometric random walk
-          berr = 0.5)   # Beta geometric walk noise
-
-true_init_cond <- c(S = N - i_infec,
-                    I = i_infec,
-                    R = 0)
-
-# get true trajectory
-sdeout_true <- StocSIR(true_init_cond, pars_true, T, steps)
-colnames(sdeout_true) <- c('S','I','R','B')
-
-# get initial data
-infec_counts_raw <- sdeout_true[,'I'] + rnorm(T+1, 0, sigma)
-infec_counts     <- ifelse(infec_counts_raw < 0, 0, infec_counts_raw)
-
-## parallel setup
-##
-
-nTrials <- 100
-
-numCores <- detectCores()
-cl <- makeCluster(min(numCores, nTrials))
-registerDoParallel(cl)
-
-## if2 setup
-##
-
-NP          <- 3000
-nPasses     <- 50
-coolrate    <- 0.975
-
-## initial fit
-##
-if2file <- paste(getwd(),"../../if2", "if2.cpp", sep="/")
-
-#if (FALSE) {
-sourceCpp(if2file)
-if2time <- system.time( if2data <- if2(infec_counts[1:(Tlim+1)], Tlim+1, N, NP, nPasses, coolrate) )
-
-# get parameter estimates
-
-paramdata <- data.frame( if2data$paramdata )
-names(paramdata) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
-parmeans <- colMeans(paramdata)
-names(parmeans) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
-
-# get fit state means
-fitdatafull <- data.frame(if2data$statemeans)
-names(fitdatafull) <- c("S","I","R")
-fitdatainfec <- fitdatafull$I
-#}
-
-
-## use parametric bootstrapping to generate forcasts
-##
-trajectories <- foreach( i = 1:nTrials, .combine = rbind, .packages = "Rcpp") %dopar% {
-
-	# draw new data from fitted means + normal sigma noise (obs noise)
-	est_counts_raw <- fitdatainfec + rnorm(length(fitdatainfec), 0, parmeans[['sigma']])
-	#est_counts_raw     <- ifelse(infec_counts_raw < 0, 0, infec_counts_raw)
-    est_counts     <- ifelse(est_counts_raw < 0, 0, est_counts_raw)
-
-    # refit using new data
-    rm(if2) # because stupid things get done in packages
-    sourceCpp(if2file)
-    if2time <- system.time( if2data <- if2(est_counts, Tlim+1, N, NP, nPasses, coolrate) )
-
-    paramdata <- data.frame( if2data$paramdata )
+	# unpack if2 first fit data
+	paramdata <- data.frame( if2data$paramdata )
 	names(paramdata) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
 	parmeans <- colMeans(paramdata)
 	names(parmeans) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
 
-	## generate the rest of the trajectory
+	## use parametric bootstrapping to generate forcasts
 	##
+	trajectories <- foreach( i = 1:nTrials, .combine = rbind, .packages = "Rcpp") %do% {
 
-	# pack new parameter estimates
+		source(stoc_sir_file)
 
-	pars <- with( 	as.list(parmeans),
-	              c(R0 = R0,
-          			r = r,
-          			N = N,
-          			eta = eta,
-          			berr = berr) )
+		## draw new data
+		##
 
-	init_cond <- c(S = if2data$statemeans[dim(if2data$statemeans)[1],1],
-                   I = if2data$statemeans[dim(if2data$statemeans)[1],2],
-                   R = if2data$statemeans[dim(if2data$statemeans)[1],3])
+		pars <- with( as.list(parmeans),
+		              c(R0 = R0,
+	          			r = r,
+	          			N = N,
+	          			eta = eta,
+	          			berr = berr) )
 
-	# generate trajectory
+		init_cond <- with( as.list(parmeans),
+		                   c(S = Sinit,
+	                   		 I = Iinit,
+	                   		 R = Rinit) )
 
-	sdeout <- StocSIR(init_cond, pars, T-Tlim-1, steps)
-	colnames(sdeout) <- c('S','I','R','B')
+		# generate trajectory
+		sdeout <- StocSIR(init_cond, pars, Tlim + 1, steps)
+		colnames(sdeout) <- c('S','I','R','B')
 
-	return( c( counts = sdeout[,'I'],
-	       		parmeans,
-	       		time = if2time[['user.self']]) )
+		# add noise
+		counts_raw <- sdeout[,'I'] + rnorm(dim(sdeout)[1], 0, parmeans[['sigma']])
+	    counts     <- ifelse(counts_raw < 0, 0, counts_raw)
 
+	    ## refit using new data
+	    ##
+
+	    rm(if2) # because stupid things get done in packages
+	    sourceCpp(if2file)
+	    if2time <- system.time( if2data <- if2(counts, Tlim+1, N, NP, nPasses, coolrate) )
+
+	    paramdata <- data.frame( if2data$paramdata )
+		names(paramdata) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
+		parmeans <- colMeans(paramdata)
+		names(parmeans) <- c("R0", "r", "sigma", "eta", "berr", "Sinit", "Iinit", "Rinit")
+
+		## generate the rest of the trajectory
+		##
+
+		# filter over parameter states
+	    if2_s_file <- paste(getwd(),"../../if2", "if2-s.cpp", sep="/")
+	    sourceCpp(if2_s_file)
+	    fixed_params <- parmeans
+	    if2time <- system.time( if2statedata <- if2_s(counts, Tlim, N, NP, coolrate, fixed_params) )
+
+	    # unpack filter results
+	    statedata <- data.frame( if2statedata$statedata )
+		names(statedata) <- c("S","I","R","B")
+		statemeans <- colMeans(statedata)
+		names(statemeans) <- c("S","I","R","B")
+
+
+		# pack new parameter estimates
+		pars <- with( 	as.list(parmeans),
+		              c(R0 = R0,
+	          			r = r,
+	          			N = N,
+	          			eta = eta,
+	          			berr = berr) )
+		init_cond <- c(S = statemeans[['S']],
+	                   I = statemeans[['I']],
+	                   R = statemeans[['R']])
+
+		# generate remaining trajectory part
+		sdeout_future <- StocSIR(init_cond, pars, T-Tlim, steps)
+		colnames(sdeout_future) <- c('S','I','R','B')
+
+		return ( c( counts = unname(sdeout_future[,'I']),
+		       	    parmeans,
+		       	    time = if2time[['user.self']]) )
+
+
+	}
+
+	return(trajectories)
 
 }
-
-
-stopCluster(cl)
-
-save.image(file = "if2-paraboot.RData")
