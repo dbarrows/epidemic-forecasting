1fe2fc83ca2b9d697a3f024f0462eb1fd019378f
Dexter Barrows <dex.barrows@gmail.com>
2016-02-08 17:17:21 -0500

    StocSIRS core model added

--- code/sir-functions/StocSIRS.r
+++ code/sir-functions/StocSIRS.r
@@ -4,11 +4,10 @@ StocSIRS <- function(y, pars, T, steps) {
 
 	R0 <- pars[['R0']]
 	r <- pars[['r']]
-	gam <- pars[['gam']]
 	N <- pars[['N']]
 	eta <- pars[['eta']]
 	berr <- pars[['berr']]
-    fr <- pars[['fr']]
+    re <- pars[['re']]
 
 	S <- y[['S']]
 	I <- y[['I']]
@@ -23,17 +22,18 @@ StocSIRS <- function(y, pars, T, steps) {
 
 	for ( i in 1:(T*steps) ) {
 
-        Bfac <- 1/2 - cos((2*pi/365)*i)/2
+        #Bfac <- 1/2 - cos((2*pi/365)*i)/2
+        Bfac <- exp(2*cos((2*pi/365)*i) - 2)
 
 		B <- exp( log(B) + eta*(log(B0) - log(B)) + rnorm(1, 0, berr) )
 
-		BSI <- Bfac * B*S*I
+		BSI <- Bfac*B*S*I
 		rI <- r*I
-        frR <- fr*R 
+        reR <- re*R 
 
-		dS <- -BSI + frR
+		dS <- -BSI + reR
 		dI <- BSI - rI
-		dR <- rI - frR
+		dR <- rI - reR
 
 		S <- S + h*dS  #newInf
 		I <- I + h*dI  #newInf - h*dR
@@ -54,7 +54,7 @@ StocSIRS <- function(y, pars, T, steps) {
 # i_infec 	<- 5
 # steps 	<- 7
 # N 		<- 500
-# sigma 	<- 10
+# sigma 	<- 5
 #
 # pars <- c(R0 = 3.0, 	# new infected people per infected person
 #           r = 0.1, 	# recovery rate
@@ -62,4 +62,4 @@ StocSIRS <- function(y, pars, T, steps) {
 #		    N = 500,    # population size
 #		    eta = 0.5, 	# geometric random walk
 #		    berr = 0.5, # Beta geometric walk noise
-#           fr = 0.1)   # resuceptibility rate
+#           re = 0.05)  # resuceptibility rate
