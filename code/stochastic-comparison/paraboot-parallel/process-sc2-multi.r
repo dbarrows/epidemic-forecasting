30902030ceb65134c9630a1534441d6c098578f1
Dexter Barrows <dex.barrows@gmail.com>
2016-02-15 23:17:12 -0500

    Process across all trial data instead of means directly

--- code/stochastic-comparison/paraboot-parallel/process-sc2-multi.r
+++ code/stochastic-comparison/paraboot-parallel/process-sc2-multi.r
@@ -3,10 +3,14 @@ library(ggplot2)
 load("sc2-multi.RData")
 
 ssedata <- data.frame(SSEmat)
-names(ssedata) <- c("IF2","HMCMC")
+if2mat <- ssedata[,1:nTrials]
+if2means <- rowMeans(if2mat, na.rm = TRUE)
+hmcmat <- ssedata[,(nTrials+1):(2*nTrials)]
+hmcmeans <- rowMeans(hmcmat, na.rm = TRUE)
+
 
 times <- 1:dim(ssedata)[1]
 
-qplot(times, log(ssedata$IF2), geom = "line", xlab = "Truncation", ylab = "log(SSE)") +
-	geom_line(aes(y = log(ssedata$HMCMC)), linetype = "dashed") +
+qplot(times, log(if2means), geom = "line", xlab = "Truncation", ylab = "log(SSE)") +
+	geom_line(aes(y = log(hmcmeans)), linetype = "dashed") +
 	theme_bw()
