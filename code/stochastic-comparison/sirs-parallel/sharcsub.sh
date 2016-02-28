bacab590334012571336547a152805db8215a4cb
Dexter Barrows <dex.barrows@gmail.com>
2016-02-27 19:24:47 -0500

    Update SIRS simulation parameters, required memory

--- code/stochastic-comparison/sirs-parallel/sharcsub.sh
+++ code/stochastic-comparison/sirs-parallel/sharcsub.sh
@@ -2,8 +2,8 @@
 
 # maximum truncation level and
 # number of trials at each trunction level
-MAXTRUNC=10
-NTRIALS=2
+MAXTRUNC=52
+NTRIALS=10
 
 basedir="/work/barrowdd"
 
@@ -44,6 +44,6 @@ for TRUNC in $(seq 1 $MAXTRUNC); do
 			sed -e "s/TRIAL/$TRIAL/" \
 				-e "s/TRUNC/$TRUNC/" > $srcfile
 		#R CMD BATCH $srcfile $outfile "--args outdir=$rdsdir"
-		sqsub -q serial -o "$soutdir/$filebase.%J.out" --mpp 500M -r 1m R CMD BATCH "--args outdir=$rdsdir" $srcfile $outfile
+		sqsub -q serial -o "$soutdir/$filebase.%J.out" --mpp 2.5G -r 5h R CMD BATCH "--args outdir=$rdsdir" $srcfile $outfile
 	done
 done
