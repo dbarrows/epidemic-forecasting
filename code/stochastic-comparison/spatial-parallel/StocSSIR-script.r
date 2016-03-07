fe6f0e5c253568dc4a0f8708626f42f722ce4d70
Dexter Barrows <dex.barrows@gmail.com>
2016-03-07 11:20:49 -0500

    Update spatial plot script and plots

--- code/stochastic-comparison/spatial-parallel/StocSSIR-script.r
+++ code/stochastic-comparison/spatial-parallel/StocSSIR-script.r
@@ -1,14 +1,16 @@
 library(ggplot2)
 library(gridExtra)
 library(reshape2)
+library(RColorBrewer)
 
 source(paste(getwd(), "../../", "sir-functions/StocSSIR.r", sep="/"))
-source("multiplot.r")
 
-T 		<- 60
+greypal <- rev( brewer.pal(9, "Greys")[-1] )
+
+T 		<- 100
 N 		<- 500
 i_infec <- 5
-nloc  	<- 10
+nloc  	<- 8
 steps   <- 7
 sigma   <- 10
 
@@ -44,14 +46,34 @@ df <- data.frame(time = 0:T, loc = t(ssdeout[,'I',]))
 plotdata <- melt(df, id = "time")
 
 ## full
-qplot(data = plotdata, x = time, y = value, geom = "line", color = variable) + theme_bw()
+q <- qplot(data = plotdata, x = time, y = value, geom = "line", color = variable, xlab = "Time", ylab = "Infection Count") +
+		scale_colour_manual(values = greypal,
+		                    labels = paste(1:8),
+		                    name = "Location") +
+		theme_bw()
+
+ggsave(q, filename="dataplot.pdf", height=4, width=6.5)
+
+infec_counts_raw <- t(ssdeout[,'I',]) + matrix( rnorm( rep(NA, (T+1)*nloc) , 0, sigma), T+1, nloc)
+infec_counts 	 <- ifelse(infec_counts_raw < 0, 0, infec_counts_raw)
+
+df2 <- data.frame(time = 0:T, loc = infec_counts)
+plotdata2 <- melt(df2, id = "time")
+
+qd <- qplot(data = plotdata2, x = time, y = value, geom = "line", color = variable, xlab = "Time", ylab = "Infection Count") +
+		scale_colour_manual(values = greypal,
+		                    labels = paste(1:8),
+		                    name = "Location") +
+		theme_bw()
+
+ggsave(qd, filename="dataplot2.pdf", height=4, width=6.5)	
 
 ## 1 v. 6, no bridge
-quartz()
-qplot(0:T, ssdeout[1,'I',], geom = "line") +
-	geom_line(aes(y = ssdeout[6,'I',])) +
-	theme_bw()
+q2 <- qplot(0:T, ssdeout[1,'I',], geom = "line") +
+		geom_line(aes(y = ssdeout[6,'I',])) +
+		theme_bw()
 
+print(q2)
 
 ## 1 v. 6 bridge
 
@@ -61,7 +83,8 @@ neibmat[1,3] <- 6
 
 ssdeout <- StocSSIR(initcond, pars, T, steps, neinum, neibmat) 
 
-quartz()
-qplot(0:T, ssdeout[1,'I',], geom = "line") +
-	geom_line(aes(y = ssdeout[6,'I',])) +
-	theme_bw()
+q3 <- qplot(0:T, ssdeout[1,'I',], geom = "line") +
+		geom_line(aes(y = ssdeout[6,'I',])) +
+		theme_bw()
+
+print(q3)
