e5b5279795450a38a58f2aa882d8d33f3ddfb15f
Dexter Barrows <dex.barrows@gmail.com>
2016-03-08 20:48:24 -0500

    SIRS test data

--- code/stochastic-comparison/sirs-parallel/test/sharcsub.sh
+++ code/stochastic-comparison/sirs-parallel/test/sharcsub.sh
@@ -31,8 +31,8 @@ if [ ! -d $soutdir ]; then
 	mkdir $soutdir
 fi
 
-#module unload intel
-#module load r
+module unload intel
+module load r
 
 # create source files and submit
 for TRIAL in $(seq $TRIALMIN $TRIALMAX); do
