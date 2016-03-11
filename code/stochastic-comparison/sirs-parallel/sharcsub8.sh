bcdd9574d2f313fbc7263ee2c45ed477394feebf
Dexter Barrows <dex.barrows@gmail.com>
2016-03-11 11:02:00 -0500

    Temp sharcsub8 script

---
+++ code/stochastic-comparison/sirs-parallel/sharcsub8.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# maximum truncation level and
+# number of trials at each trunction level
+TRIALMIN=1
+TRIALMAX=20
+
+basedir="/work/barrowdd"
+
+# source files directory
+srcdir="$basedir/src"
+if [ ! -d  $srcdir ]; then
+	mkdir $srcdir
+fi
+
+# output files directory
+outdir="$basedir/out"
+if [ ! -d $outdir ]; then
+	mkdir $outdir
+fi
+
+# image files directory
+vardir="$basedir/varimages"
+if [ ! -d $varimagesdir ]; then
+	mkdir $varimagesdir
+fi
+
+# sharcnet output files directory
+soutdir="$basedir/sout"
+if [ ! -d $soutdir ]; then
+	mkdir $soutdir
+fi
+
+#module unload intel
+#module load r
+
+# create source files and submit
+for TRIAL in 8; do
+	filebase="fsim-$TRIAL"
+	srcfile="$srcdir/$filebase.r"
+	outfile="$outdir/$filebase.Rout"
+	cat fsimthread.r | sed -e "s/TRIAL/$TRIAL/" > $srcfile
+	sqsub -q serial -o "$soutdir/$filebase.%J.out" --mpp 2.5G -r 20h Rscript $srcfile $vardir
+done
